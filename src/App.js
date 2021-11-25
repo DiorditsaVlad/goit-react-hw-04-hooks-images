634b6ffdf4f8fe6e5720ec8f70504ef6cf192a5d
DiorditsaVlad <79572511+DiorditsaVlad@users.noreply.github.com>
2021-11-25 18:28:58 +0300

    fixed-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Searcbar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Button from "./components/Button/Button";
+import Loader from "react-loader-spinner";
+import Modal from "./components/Modal/Modal";
+import imageApi from "./components/services";
 import "./App.css";
 
 export default function App() {
   const [searchbar, setSearchbar] = useState("");
+  const [status, setStatus] = useState("idle");
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState("");
+  const [data, setData] = useState([]);
+  const [modaleUrl, setModaleUrl] = useState("");
+  const [modaleAlt, setModaleAlt] = useState("");
 
   const onFormSubmit = (searchName) => {
     if (searchName.trim !== "") {
       setSearchbar(searchName);
+      setStatus("pending");
     }
     return;
   };
 
+  useEffect(() => {
+    setStatus("pending");
+    if (searchbar.trim() === "") {
+      setStatus("idle");
+      return;
+    }
+
+    imageApi
+      .fatchImage(searchbar, page)
+      .then((images) => {
+        if (images.hits.length === 0) {
+          setError("No any picture");
+          setStatus("rejected");
+          return;
+        } else {
+          setData((prevState) => [...prevState, ...images.hits]);
+          setStatus("resolved");
+        }
+      })
+      .then(() => {
+        if (page !== 1) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth",
+          });
+        }
+      })
+      .catch((error) => setError(error), setStatus("rejected"));
+  }, [page, searchbar]);
+
+  const loadMore = () => {
+    setPage((prevState) => prevState + 1);
+    setStatus("pending");
+  };
+
+  const modalOpen = (modaleUrl, modaleAlt) => {
+    window.addEventListener("keydown", cleanEventListener);
+    setModaleUrl(modaleUrl);
+    setModaleAlt(modaleAlt);
+  };
+  const modalClose = () => {
+    window.removeEventListener("keydown", cleanEventListener);
+    setModaleUrl("");
+    setModaleAlt("");
+  };
+
+  const cleanEventListener = (e) => {
+    if (e.code === "Escape") {
+      modalClose();
+    }
+  };
   return (
     <div className="App">
       <Searcbar submit={onFormSubmit} />
-      <ImageGallery searchbar={searchbar} />
+      {status === "idle" && <p>Enter the name of the picture</p>}
+      <div>
+        <ImageGallery modalOpen={modalOpen} data={data} />
+        {status === "resolved" && <Button onClick={loadMore} />}
+      </div>
+      {status === "pending" && (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      )}
+      {modaleUrl && (
+        <Modal largeImageURL={modaleUrl} alt={modaleAlt} onClick={modalClose} />
+      )}
+      {status === "rejected" && <p>{error}</p>}
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,92 +1,15 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
-import Button from "../Button/Button";
+// import Button from "../Button/Button";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
-import Modal from "../Modal/Modal";
+// import Loader from "react-loader-spinner";
+// import Modal from "../Modal/Modal";
 import s from "./ImageGallery.module.css";
-import imageApi from "../services";
+// import imageApi from "../services";
 
-export default function ImageGallery({ searchbar }) {
-  const [status, setStatus] = useState("idle");
-  const [page, setPage] = useState(1);
-  const [error, setError] = useState("");
-  const [data, setData] = useState([]);
-  const [modaleUrl, setModaleUrl] = useState("");
-  const [modaleAlt, setModaleAlt] = useState("");
-
-  // useEffect(() => {
-  //   setData([]);
-  // }, [searchbar]);
-
-  useEffect(() => {
-    setStatus("pending");
-    if (searchbar.trim() === "") {
-      setStatus("idle");
-      return;
-    }
-
-    imageApi
-      .fatchImage(searchbar, page)
-      .then((images) => {
-        if (images.hits.length === 0) {
-          setError("No any picture");
-          setStatus("rejected");
-          return;
-        } else {
-          setData((prevState) => [...prevState, ...images.hits]);
-          setStatus("resolved");
-        }
-      })
-      .then(() => {
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: "smooth",
-        });
-      })
-      .catch((error) => setError(error), setStatus("rejected"));
-  }, [page, searchbar]);
-
-  const loadMore = () => {
-    setPage((prevState) => prevState + 1);
-    setStatus("pending");
-  };
-
-  const modalOpen = (modaleUrl, modaleAlt) => {
-    window.addEventListener("keydown", cleanEventListener);
-    setModaleUrl(modaleUrl);
-    setModaleAlt(modaleAlt);
-  };
-  const modalClose = () => {
-    window.removeEventListener("keydown", cleanEventListener);
-    setModaleUrl("");
-    setModaleAlt("");
-  };
-
-  const cleanEventListener = (e) => {
-    if (e.code === "Escape") {
-      modalClose();
-    }
-  };
-
-  if (status === "idle") {
-    return <p>Enter the name of the picture</p>;
-  }
-
-  if (status === "pending") {
-    return (
-      <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
-    );
-  }
-
-  if (status === "resolved") {
-    return (
+export default function ImageGallery({ modalOpen, data }) {
+  return (
+    <>
       <ul className={s.ImageGallery}>
         {data.map((image) => {
           return (
@@ -99,18 +22,7 @@ export default function ImageGallery({ searchbar }) {
             />
           );
         })}
-        <Button onClick={loadMore} />
-        {modaleUrl && (
-          <Modal
-            largeImageURL={modaleUrl}
-            alt={modaleAlt}
-            onClick={modalClose}
-          />
-        )}
       </ul>
-    );
-  }
-  if (status === "rejected") {
-    return <p>{error}</p>;
-  }
+    </>
+  );
 }
